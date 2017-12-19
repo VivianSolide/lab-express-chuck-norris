6c2e98e6f63f2edadc07977ae9bb24ff2ca5b89d
Viviane <viviane@MacBook-Pro-de-Mickael-Bolnet.local>
2017-12-19 22:16:01 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,40 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response, next) => {
+    response.render('index');
+});
+
+app.get('/random', (req, res, next) => {
+    client.getRandomJoke()
+        .then((response) => {
+            res.render('random', {
+                response : response.value
+            });
+        }).catch((err) => {
+            console.log(err);
+    });
+});
+
+app.get('/categories', (req, res, next) => {
+    client.getJokeCategories()
+    .then((response)=>  {
+        res.render('categories', {
+            categories : response,
+        });
+    })
+    .catch((err)=> {
+      // handle error
+    });
+});
+
+// app.get('/search', (request, response, next) => {
+//     response.render('hello world');
+// });
+
+app.listen(3000);
